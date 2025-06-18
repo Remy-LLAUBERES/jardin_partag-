917290638de6e90f1d89ba1ce5964287cc4c5490
Remy-LLAUBERES <remy.llauberes@gmail.com>
2025-06-18 11:17:33 +0200

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,7 +1,24 @@
-self.addEventListener('install', event => {
-  self.skipWaiting();
+self.addEventListener('install', function(e) {
+  e.waitUntil(
+    caches.open('jardin-cache').then(function(cache) {
+      return cache.addAll([
+        '/',
+        './index.html',
+        './ajout_plantation.html',
+        './fiche_plantation.html',
+        './stock_materiel.html',
+        './stock_plant.html',
+        './favicon.png'
+      ]);
+    })
+  );
 });
 
-self.addEventListener('fetch', event => {
-  event.respondWith(fetch(event.request).catch(() => caches.match(event.request)));
+self.addEventListener('fetch', function(e) {
+  e.respondWith(
+    caches.match(e.request).then(function(response) {
+      return response || fetch(e.request);
+    })
+  );
 });
+.catch(err => console.error("Erreur de mise en cache", err));
